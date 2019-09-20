0da345d2b03840f1033677578f47051360136c67
Anders Børjesson <anbo@zealand.dk>
2019-09-20 13:50:33 +0200

    class Reservation

---
+++ RoomReservationV3/model/Reservation.cs
@@ -0,0 +1,28 @@
+﻿namespace RoomReservationV3.model
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+
+        // https://www.unixtimestamp.com/
+        // https://stackoverflow.com/questions/17632584/how-to-get-the-unix-timestamp-in-c-sharp
+        public int FromTime { get; set; }
+
+        // constraint: FromTime <= ToTime
+        public int ToTime { get; set; }
+        public string UserId { get; set; }
+        public string Purpose { get; set; }
+
+        // constraint: must refer to an existing RoomId
+        public int RoomId { get; set; }
+
+        // public string DeviceId { get; set; }
+
+        public bool Intersects(Reservation other) // todo static?
+                                                  // todo test Intersects
+        {
+            return (FromTime < other.FromTime && other.FromTime < ToTime) ||
+                   (FromTime < other.ToTime && other.ToTime < ToTime);
+        }
+    }
+}
