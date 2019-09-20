806a7de46612ec3f2588f2d8dacc51b50847d8d2
Anders Børjesson <anbo@zealand.dk>
2019-09-20 13:48:26 +0200

    class Room

---
+++ RoomReservationV3/model/Room.cs
@@ -0,0 +1,11 @@
+﻿namespace RoomReservationV3.model
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Capacity { get; set; }
+        public string Remarks { get; set; }
+    }
+}
